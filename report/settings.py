1fabc92455a5697ade047d5ee50d5c9a4f3a67a9
lilue <aa_0933@qq.com>
2022-02-11 10:57:31 +0800

    test pull

--- report/settings.py
+++ report/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+# 测试一下有没有自动更新
 import os
 import environ
 
