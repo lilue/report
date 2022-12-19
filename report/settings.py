a2251679210e41162bec38cb1f420960b1210bea
lilue <aa_0933@qq.com>
2022-12-19 14:57:53 +0800

    update setting ALLOWED_HOSTS

--- report/settings.py
+++ report/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = env.str('SECRET_KEY', '3*v+j3(#kp8t&d9e5*fgi8qipzlwtgfo#qw40hs!2+_f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', False)
 
-# ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
-ALLOWED_HOSTS = "*"
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+# ALLOWED_HOSTS = "*"
 
 # Application definition
 
