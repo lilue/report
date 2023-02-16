cde4f18846e2ee0a58a6479c6a7e522a3399a00c
lilue <aa_0933@qq.com>
2023-02-16 15:33:42 +0800

    check date

--- nucleic/tests.py
+++ nucleic/tests.py
@@ -1,3 +1,26 @@
+import uuid
 from django.test import TestCase
-
 # Create your tests here.
+from nucleic.models import Payment, QRconfig
+
+
+class DtestCase(TestCase):
+    # username = '测试人员1'
+    # people = '1'
+    # amount = 100
+    # openid = 'oyNgwwrZczcklZWefVNF15Ii2Ioc'
+    # card = '440825199909096789'
+    # phone = '13888888888'
+    # paySign = 'D326582A40E06E28DE0834D683B6D58D'
+    # expTime = '2022-11-15 17:48:12.720399'
+    # nonce_str = 'MaH9ek68Vc5uWLUz'
+    # prepay_id = 'wx15161812637247523c10bbb37edfe40000'
+    # timestamp = '1668500292'
+    # order_no = ''.join(str(uuid.uuid1()).split('-'))
+    # dd = QRconfig.objects.get(location='医院')
+    # pay_order = Payment.objects.create(patient=username, number=people, amount=amount, openid=openid, idcard=card,
+    #                                    paySign=paySign, qr_id=dd.id, expDate=expTime, orderNo=order_no, phone=phone,
+    #                                    preNonce=nonce_str, prepayId=prepay_id, preTime=timestamp)
+    # print(pay_order)
+    pass
+
--- wechat/tests.py
+++ wechat/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
 
 # Create your tests here.
+from reports.models import Report
+from utils.process import process_date
+
+
+class WXtestCase(TestCase):
+    symbol = '*'
+    msg = '15014424054*440803195801242921'
+    text = msg.split(symbol, 1)
+    query_set = Report.objects.filter(phone=text[0], idCard=text[1]).order_by('-id')[:1]
+    if query_set.exists():
+        # 有数据
+        template = "【新型冠状病毒(COVID-19)核酸检测结果】\n" \
+                   "姓名：%s\n" \
+                   "采样机构：%s\n" \
+                   "检测机构：湛江市坡头区人民医院\n" \
+                   "检测日期：%s\n" \
+                   "检测结果：%s\n" \
+                   "此报告仅对所检验标本负责，如有疑议请在三天内与检验科联系！\n" \
+                   "PDF版报告：【%s】, 请复制至浏览器打开。"
+        for report in query_set:
+            if not report.zjg:
+                zjg = '阴性(-)'
+            else:
+                zjg = report.zjg
+            print(report.inspection_date)
+            break
+            str_ss = report.inspection_date.split(' ', 1)
+            folder = process_date(report.inspection_date)
+            pdfUrl = "https://image.zhonghefull.com/pdf/%s/%s.pdf" % (folder, report.idCard)
+            result = template % (report.name, report.hospital, str_ss[0], zjg, pdfUrl)
+    print(result)
